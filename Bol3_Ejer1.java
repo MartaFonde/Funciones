1a409f494e097adb3a7507763881f2c61aa032ac
Marta Fondevila <55930814+MartaFonde@users.noreply.github.com>
2019-10-30 22:00:25 +0100

    Add files via upload

---
+++ Bol3_Ejer1.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Bol3_Ejer1 {
+/**
+ * Imprime en pantalla N lineas en blanco 
+ * 
+ * @param N Especifica numero de lineas
+ */
+    static void lineasBlanco (int n) {
+        for(int i=1; i<=n; i++) {
+            System.out.print(" \n");
+        }       
+    }
+/**
+ * 
+ * Analiza si un numero entero es par 
+
+ * @param num validar si es entero
+ * 
+ * @return Devuelve true si el parametro es numero par y false si no lo es
+ */
+    static boolean par (int num) {
+        if (num%2==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //return(num%2==0? true: false);
+    // variables booleanas, son resultado de una operación lógica que da valores true o false
+
+/**
+ * Evalua si un numero entero es par o impar 
+ * 
+ * @param num a evaluar signo
+ * 
+ * @return devuelve la letra P si el numero es positivo y la letra N si el numero es cero o negativo
+ */
+    static char signo (int num) {
+        return num>0? 'P': 'N';             
+    }
+
+    public static void main (String args[]) {   
+        Scanner sc=new Scanner(System.in);
+        int n;
+        int num;
+        boolean numPar;
+        char numSigno;
+
+        System.out.println("¿Cuántas líneas en blanco quiere imprimir?");
+        n=sc.nextInt();
+        lineasBlanco(n);    //lineasBlanco(sc.nextInt()); sin int=N en main
+
+        System.out.println("Escribe un número par");
+        num=sc.nextInt();
+        numPar=par(num);
+        numSigno=signo(num);
+        
+        System.out.println(numPar);
+        System.out.println(numSigno);
+
+    }        
+    
+} 
---
+++ Bol3_Ejer2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Bol3_Ejer2 {
+
+    /**
+     * Funcion que analiza si un año es bisiesto o no lo es
+     * 
+     * @param año a evaluar si es bisiesto o no
+     * 
+     * @return la funcion devuelve true si el año es bisiesto y false si no lo es
+    */
+    
+
+    static boolean bisiesto (int año) {
+        if (año%4==0 && año%100!=0  || año%400==0){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    // static boolean bisiesto (int año) {
+       // return año%4==0 && año%100!=0  || año%400==0;
+       // }
+
+    public static void main (String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int año;
+        boolean añoBis;
+        
+        do {
+        System.out.println("Escribe un año bisiesto");
+        año=sc.nextInt();
+
+        if (año>0) {
+            añoBis=bisiesto(año);
+            System.out.println(añoBis);                
+        }       
+        } while (año!=0);
+        
+    }
+}
